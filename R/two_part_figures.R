69e5270515f24df00593dd1827935463ca2a1e5f
Jason Venkiteswaran <jason.venkiteswaran@gmail.com>
2016-06-01 10:21:12 -0400

    Add "na.rm = TRUE" to the plotting functions to remove the warnings about missing values.

--- R/two_part_figures.R
+++ R/two_part_figures.R
@@ -70,16 +70,18 @@ two_part_figure_all <- function(ysim) {
 two_part_figure_with_obs <- function(ysim, yobs, obstime) {
   g.top <- ggplot2::ggplot(reshape2::melt(subset(ysim, select = c(time, NO3, TAN, DIN)), id.vars = "time"),
                            ggplot2::aes(x = time, y = value, colour = variable)) +
-    ggplot2::geom_line(size = 1) +
-    ggplot2::geom_point(data = reshape2::melt(subset(plyr::rename(cbind(obstime, yobs), c(obstime = "time")), select = c(time, NO3, TAN, DIN)), id.vars = "time"), size = 4) +
+    ggplot2::geom_line(size = 1, na.rm = TRUE) +
+    ggplot2::geom_point(data = reshape2::melt(subset(plyr::rename(cbind(obstime, yobs), c(obstime = "time")), select = c(time, NO3, TAN, DIN)), id.vars = "time"),
+                        size = 4, na.rm = TRUE) +
     ggplot2::labs(x = "", y = "Concentration (mgN/L)", colour = "") +
     ggplot2::theme(legend.position = "none", legend.box = "horizontal") +
     ggplot2::scale_y_continuous(limits = c(0, max(ysim$DIN, rm.na = TRUE) + 1)) +
     ggplot2::scale_colour_discrete(labels = c(expression(NO[3]^"-"), expression(TAN), expression(DIN)))
   g.bottom <- ggplot2::ggplot(reshape2::melt(subset(ysim, select = c(time, deltaNO3, deltaTAN, deltaDIN)), id.vars = "time"),
                               ggplot2::aes(x = time, y = value, colour = variable)) +
-    ggplot2::geom_line(size = 1) +
-    ggplot2::geom_point(data = reshape2::melt(subset(plyr::rename(cbind(obstime, yobs), c(obstime = "time")), select = c(time, deltaNO3, deltaTAN, deltaDIN)), id.vars = "time"), size = 4) +
+    ggplot2::geom_line(size = 1, na.rm = TRUE) +
+    ggplot2::geom_point(data = reshape2::melt(subset(plyr::rename(cbind(obstime, yobs), c(obstime = "time")), select = c(time, deltaNO3, deltaTAN, deltaDIN)), id.vars = "time"),
+                        size = 4, na.rm = TRUE) +
     ggplot2::labs(x = "Distance (m)", y = expression("\u3b4"^{15}*"N (\u2030 vs air)"), colour = "") +
     ggplot2::theme(legend.position = "bottom", legend.box = "horizontal") +
     ggplot2::scale_y_continuous(limits = c(0, 40)) + ggplot2::scale_colour_discrete(labels = c(expression(NO[3]^"-"), expression(TAN), expression(DIN)))
